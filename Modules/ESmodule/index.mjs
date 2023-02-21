0e5c67dde332061e2e170a87684c134ba1662154
Samiul Khan <toughlifesamiul@gmail.com>
2023-02-21 21:55:40 +0530

    21/02/2023

---
+++ Modules/ESmodule/index.mjs
@@ -0,0 +1,2 @@
+import {add,del,sub} from './module.mjs';
+console.log(add(1,2));
---
+++ Modules/ESmodule/module.mjs
@@ -0,0 +1,14 @@
+export const add = (a,b)=>{
+    return a+b;
+}
+
+export const sub = (a,b)=>{
+    return a+b;
+}
+export const del = (a,b)=>{
+    return a+b;
+}
+
+
+
+// export default add;
