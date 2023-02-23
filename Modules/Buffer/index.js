d3410609beff1305d767ff648c3262c179d65801
Samiul Khan <toughlifesamiul@gmail.com>
2023-02-23 21:42:23 +0530

    23/02/2023

---
+++ Modules/Buffer/index.js
@@ -0,0 +1,21 @@
+const buffer = new Buffer.from("samiulhgu");
+// 1stpart
+// console.log(buffer);
+// console.log(buffer.toJSON());
+// console.log(buffer.toString());
+
+
+
+
+// 2ndpart
+// buffer.write("hjjjj");
+// console.log(buffer);
+// console.log(buffer.toJSON());
+// console.log(buffer.toString());
+
+
+
+
+
+
+
