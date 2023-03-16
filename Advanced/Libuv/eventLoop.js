4f50c3db0bb2792d906880d404b034f65ce0d1e7
Samiul Khan <toughlifesamiul@gmail.com>
2023-03-16 22:16:47 +0530

    16/03/2023

---
+++ Advanced/Libuv/eventLoop.js
@@ -0,0 +1,270 @@
+//Microtask Queue
+//1
+// console.log("1");
+// process.nextTick(() => {
+//   console.log("i am process 1");
+// });
+// console.log("2");
+/**
+ * 1
+ * 2
+ * i am process 1
+ */
+
+//2
+// console.log("1");
+// Promise.resolve().then(() => {
+//   console.log("i am a promise");
+// });
+// console.log("2");
+//3
+// process.nextTick(() => {
+//   console.log("i am process 1");
+// });
+// Promise.resolve().then(() => {
+//   console.log("i am  promise 1");
+// });
+//---
+// Promise.resolve().then(() => {
+//   console.log("i am  promise 1");
+// });
+// process.nextTick(() => {
+//   console.log("i am process 1");
+// });
+
+//4
+// next =
+// Promise =
+// this is process 1
+// this is process 2
+// this is process 3
+// this is the inner next tick inside next tick
+// this is Promise.resolve 1
+// this is Promise.resolve 2
+// this is Promise.resolve 3
+// this is the inner next tick inside Promise then block
+
+// process.nextTick(() => console.log("this is process 1"));
+// process.nextTick(() => {
+//   console.log("this is process 2");
+//   process.nextTick(() =>
+//     console.log("this is the inner next tick inside next tick")
+//   );
+// });
+// process.nextTick(() => console.log("this is process 3"));
+// Promise.resolve().then(() => console.log("this is Promise.resolve 1"));
+// Promise.resolve().then(() => {
+//   console.log("this is Promise.resolve 2");
+//   process.nextTick(() =>
+//     console.log("this is the inner next tick inside Promise then block")
+//   );
+// });
+// Promise.resolve().then(() => console.log("this is Promise.resolve 3"));
+
+//Microtask and timer
+
+/**
+ 
+ Timer =
+ next = 
+ promise = 
+ this is process.nextTick 1
+ this is process.nextTick 2
+ this is process.nextTick 3
+ this is the inner next tick inside next tick
+ this is Promise.resolve 1
+ this is Promise.resolve 2
+ this is Promise.resolve 3
+ this is the inner next tick inside Promise then block
+ this is setTimeout 1
+ this is setTimeout 2
+ this is setTimeout 3
+ */
+
+// setTimeout(() => console.log("this is setTimeout 2"), 0);
+// setTimeout(() => console.log("this is setTimeout 1"), 0);
+// setTimeout(() => console.log("this is setTimeout 3"), 0);
+
+// process.nextTick(() => console.log("this is process.nextTick 1"));
+// process.nextTick(() => {
+//   console.log("this is process.nextTick 2");
+//   process.nextTick(() =>
+//     console.log("this is the inner next tick inside next tick")
+//   );
+// });
+// process.nextTick(() => console.log("this is process.nextTick 3"));
+// Promise.resolve().then(() => console.log("this is Promise.resolve 1"));
+// Promise.resolve().then(() => {
+//   console.log("this is Promise.resolve 2");
+//   process.nextTick(() =>
+//     console.log("this is the inner next tick inside Promise then block")
+//   );
+// });
+// Promise.resolve().then(() => console.log("this is Promise.resolve 3"));
+
+//--
+/**
+ Timer = 
+ next = 
+ promise = 
+this is process.nextTick 1
+this is process.nextTick 2
+his is the inner next tick inside next tick
+this is Promise.resolve 1
+this is Promise.resolve 2
+this is Promise.resolve 3
+ this is the inner next tick inside Promise then block
+ this is setTimeout 1
+this is setTimeout 2
+this is the inner next tick inside setTimeout
+this is setTimeout 3
+ */
+// setTimeout(() => console.log("this is setTimeout 1"), 0);
+// setTimeout(() => {
+//   console.log("this is setTimeout 2");
+//   process.nextTick(() => {
+//     console.log("this is the inner next tick inside setTimeout");
+//   });
+// }, 0);
+// setTimeout(() => console.log("this is setTimeout 3"), 0);
+// process.nextTick(() => console.log("this is process.nextTick 1"));
+// process.nextTick(() => {
+//   console.log("this is process.nextTick 2");
+//   process.nextTick(() => {
+//     console.log("this is the inner next tick inside next tick");
+//   });
+// });
+// Promise.resolve().then(() => console.log("this is Promise.resolve 1"));
+// Promise.resolve().then(() => {
+//   console.log("this is Promise.resolve 2");
+//   process.nextTick(() =>
+//     console.log("this is the inner next tick inside Promise then block")
+//   );
+// });
+// Promise.resolve().then(() => console.log("this is Promise.resolve 3"));
+
+//--
+// setTimeout(() => console.log("this is setTimeout 1"), 0);
+// setTimeout(() => {
+//   console.log("this is setTimeout 2");
+//   Promise.resolve().then(() => {
+//     console.log("this is the inner promise inside setTimeout");
+//   });
+// }, 0);
+// setTimeout(() => console.log("this is setTimeout 3"), 0);
+// process.nextTick(() => console.log("this is process.nextTick 1"));
+// process.nextTick(() => {
+//   console.log("this is process.nextTick 2");
+//   process.nextTick(() => {
+//     console.log(console, "this is the inner next tick inside next tick");
+//   });
+// });
+// Promise.resolve().then(() => console.log("this is Promise.resolve 1"));
+// Promise.resolve().then(() => {
+//   console.log("this is Promise.resolve 2");
+//   process.nextTick(() =>
+//     console.log("this is the inner next tick inside Promise then block")
+//   );
+// });
+// Promise.resolve().then(() => console.log("this is Promise.resolve 3"));
+//-
+// setTimeout(() => console.log("this is setTimeout 1"), 1000);
+// setTimeout(() => console.log("this is setTimeout 2"), 500);
+// setTimeout(() => console.log("this is setTimeout 3"), 0);
+
+//Microtask ans IO queu
+
+// const fs = require("fs");
+
+// fs.readFile(__filename, () => {
+//   console.log("this is readFile 1");
+// });
+
+// process.nextTick(() => console.log("this is process.nextTick 1"));
+// Promise.resolve().then(() => console.log("this is Promise.resolve 1"));
+
+//- Timer anamoly. Order of execution can never be guaranteed
+
+// const fs = require("fs");
+// setTimeout(() => console.log("this is setTimeout 1"), 0);
+// fs.readFile(__filename, () => {
+//   console.log("this is readFile 1");
+// });
+// //-
+// const fs = require("fs");
+
+// fs.readFile(__filename, () => {
+//   console.log("this is readFile 1");
+// });
+
+// process.nextTick(() => console.log("this is process.nextTick 1"));
+// Promise.resolve().then(() => console.log("this is Promise.resolve 1"));
+// setTimeout(() => console.log("this is setTimeout 1"), 0);
+// for (let i = 0; i < 2000000000; i++) {}
+
+//-io polling
+
+// const fs = require("fs");
+
+// fs.readFile(__filename, () => {
+//   console.log("this is readFile 1");
+// });
+
+// process.nextTick(() => console.log("this is process.nextTick 1"));
+// Promise.resolve().then(() => console.log("this is Promise.resolve 1"));
+// setTimeout(() => console.log("this is setTimeout 1"), 0);
+// setImmediate(() => console.log("this is setImmidiate 1"));
+// for (let i = 0; i < 2000000000; i++) {}
+
+//Check queue
+// const fs = require("fs");
+
+// fs.readFile(__filename, () => {
+//   console.log("this is readFile 1");
+//   setImmediate(() => console.log("this is inner setImmediate inside readFile"));
+// });
+
+// process.nextTick(() => console.log("this is process.nextTick 1"));
+// Promise.resolve().then(() => console.log("this is Promise.resolve 1"));
+// setTimeout(() => console.log("this is setTimeout 1"), 0);
+
+// for (let i = 0; i < 2000000000; i++) {}
+// //-
+// const fs = require("fs");
+// fs.readFile(__filename, () => {
+//   console.log("this is readFile 1");
+//   setImmediate(() => console.log("this is inner setImmediate inside readFile"));
+//   process.nextTick(() =>
+//     console.log("this is inner process.nextTick inside readFile")
+//   );
+//   Promise.resolve().then(() =>
+//     console.log("this is inner Promise.resolve inside readFile")
+//   );
+// });
+
+// process.nextTick(() => console.log("this is process.nextTick 1"));
+// Promise.resolve().then(() => console.log("this is Promise.resolve 1"));
+// setTimeout(() => console.log("this is setTimeout 1"), 0);
+
+// for (let i = 0; i < 2000000000; i++) {}
+//-
+// setImmediate(() => console.log("this is setImmediate 1"));
+// setImmediate(() => {
+//   console.log("this is setImmediate 2");
+//   process.nextTick(() => console.log("this is process.nextTick 1"));
+//   Promise.resolve().then(() => console.log("this is Promise.resolve 1"));
+// });
+// setImmediate(() => console.log("this is setImmediate 3"));
+// //-
+// const fs = require("fs");
+
+// const readableStream = fs.createReadStream(__filename);
+// readableStream.close();
+
+// readableStream.on("close", () => {
+//   console.log("this is from readableStream close event callback");
+// });
+// setImmediate(() => console.log("this is setImmediate 1"));
+// setTimeout(() => console.log("this is setTimeout 1"), 0);
+// Promise.resolve().then(() => console.log("this is Promise.resolve 1"));
+// process.nextTick(() => console.log("this is process.nextTick 1"));
